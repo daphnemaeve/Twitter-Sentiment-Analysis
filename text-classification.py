29cdba3c4317ab7d5e0845fff0dfcae19e12983c
Maeve Hoffer <daphnemaeve@gmail.com>
2021-05-28 13:05:49 -0700

    Specify BERT model being used, load preprocessing model into a hub.KerasLayer

--- text-classification.py
+++ text-classification.py
@@ -7,12 +7,13 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
 from official.nlp import optimization
+from sklearn.model_selection import train_test_split
 
 import matplotlib.pyplot as plt
 
 tf.get_logger().setLevel('ERROR')
 
-dataset = tf.data.experimental.make_csv_dataset(
+training = tf.data.experimental.make_csv_dataset(
     "data/training.csv"
     , label_name="party"
     , header=True
@@ -20,5 +21,21 @@ dataset = tf.data.experimental.make_csv_dataset(
     , shuffle_seed=42)
 
 #print a single batch
-iterator = dataset.as_numpy_iterator()
+iterator = training.as_numpy_iterator()
 print(next(iterator))
+
+#Select BERT model and preprocessor
+bert_model = 'https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1'
+preprocessor = 'https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3'
+
+#Load preprocessor into hub.KerasLayer
+bert_preprocess_model = hub.KerasLayer(preprocessor)
+
+text_test = ['this is a democratic tweet']
+text_preprocessed = bert_preprocess_model(text_test)
+
+print(f'Keys	   : {list(text_preprocessed.keys())}')
+print(f'Shape	   : {text_preprocessed["input_word_ids"].shape}')
+print(f'Word Ids   : {text_preprocessed["input_word_ids"][0, :12]}')
+print(f'Input Mask : {text_preprocessed["input_mask"][0, :12]}')
+print(f'Type Ids   : {text_preprocessed["input_type_ids"][0, :12]}')
