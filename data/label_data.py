60356d1e2f8e5f64fdd70072b8b0268023ff1cb6
Daniel Reimer <daniel@muse.dev>
2021-05-30 18:45:17 -0700

    restructure the dataset to expected format

--- data/label_data.py
+++ data/label_data.py
@@ -5,13 +5,27 @@ from csv import reader
 from os import listdir,system
 from functools import partial
 from random import shuffle
+import sys
 
 percent = 0.80
 
 parties = ["democrat", "republican", "green", "libertarian"]
 
+def party_to_label(party):
+    if party == "democrat":
+        return 0
+    elif party == "republican":
+        return 1
+    elif party == "green":
+        return 2
+    elif party == "libertarian":
+        return 3
+    else:
+        print >> sys.stderr, ("unkown party: "+party)
+        sys.exit(1)
+
 def label_files(party,filename):
-    return [(party,csv[3]) for csv in list(reader(open(party+'/'+filename, 'r')))[1:]]
+    return [(party_to_label(party),csv[3]) for csv in list(reader(open(party+'/'+filename, 'r')))[1:]]
 
 data = [ list(map(partial(label_files,party), listdir(party)))
                         for party in parties ]
@@ -24,10 +38,10 @@ testing = data[int(size*percent):size]
 system("rm {training.csv,testing.csv} &> /dev/null")
 with open("training.csv", 'w') as csvfile:
     csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(["party","tweet"])
+    csvwriter.writerow(["party","text"])
     csvwriter.writerows(sum(sum(training,[]),[]))
 
 with open("testing.csv", 'w') as csvfile:
     csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(["party","tweet"])
+    csvwriter.writerow(["party","text"])
     csvwriter.writerows(sum(sum(testing,[]),[]))
--- text-classification.py
+++ text-classification.py
@@ -42,22 +42,22 @@ bert_results = bert_model(text_preprocessed)
 
 #define bert model
 def build_classifier_model():
-	text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
-	preprocessing_layer = hub.KerasLayer(preprocessor, name='preprocessing')
-	encoder_inputs = preprocessing_layer(text_input)
-	encoder = hub.KerasLayer(bert, trainable=True, name='BERT_encoder')
-	outputs = encoder(encoder_inputs)
-	net = outputs['pooled_output']
-	net = tf.keras.layers.Dropout(0.1)(net)
-	net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
-	return tf.keras.Model(text_input, net)
+        text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
+        preprocessing_layer = hub.KerasLayer(preprocessor, name='preprocessing')
+        encoder_inputs = preprocessing_layer(text_input)
+        encoder = hub.KerasLayer(bert, trainable=True, name='BERT_encoder')
+        outputs = encoder(encoder_inputs)
+        net = outputs['pooled_output']
+        net = tf.keras.layers.Dropout(0.1)(net)
+        net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
+        return tf.keras.Model(text_input, net)
 
 classifier_model = build_classifier_model()
 bert_raw_result = classifier_model(tf.constant(text_test))
 
 #loss function
-loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-metrics = tf.metrics.BinaryAccuracy()
+loss = tf.keras.losses.SparseCategoricalCrossentropy([0,1,2,3])
+# metrics = tf.metrics.SparseTopKCategoricalAccuracy(k=4)
 
 #optimizer
 epochs = 5
@@ -67,15 +67,15 @@ num_warmup_steps = int(0.1*num_train_steps)
 
 init_lr = 3e-5
 optimizer = optimization.create_optimizer(init_lr=init_lr,
-										  num_train_steps=num_train_steps,
-										  num_warmup_steps=num_warmup_steps,
-										  optimizer_type='adamw')
+                                          num_train_steps=num_train_steps,
+                                          num_warmup_steps=num_warmup_steps,
+                                          optimizer_type='adamw')
 
 #load model and train
 classifier_model.compile(optimizer=optimizer,
-						 loss=loss,
-						 metrics=metrics)
+                         loss=loss,
+                         metrics=['accuracy'])
 
 print(f'Training model with {bert}')
 #also can add validation dataset
-#history = classifier_model.fit(x=training, epochs=epochs)
+history = classifier_model.fit(x=training, epochs=epochs)
