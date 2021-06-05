03e6f684493ef7f283dca15b225b8cb10be02d29
Daniel Reimer <daniel@muse.dev>
2021-06-05 11:42:20 -0700

    Do more label filtering, reduce training size

--- data/label_data.py
+++ data/label_data.py
@@ -11,6 +11,9 @@ import re
 # ratio of training/testing
 percent = 0.80
 
+training_size = 10000
+testing_size = 2000
+
 parties = ["democrat", "republican", "green", "libertarian"]
 
 def party_to_label(party):
@@ -33,20 +36,26 @@ def label_files(party,filename):
 def sanitize_line(t):
     party = t[0]
     text = t[1]
-    text = text.replace('\n',' ')
-    text = text.replace('""','"')
-    text = re.sub(r"http\S+", "", text)
-    text = re.sub(r"RT .*: .*",'',text)
-    return [party,str(text)]
+    text = text.replace('\n',' ') # replace new lines with space
+    text = re.sub(r"http\S+", "", text) # remove links
+    text = re.sub(r"RT .*: .*",'',text) # remove retweets
+    text = re.sub(r"@[\w]+",'',text) # remove @user
+    text = text.replace('""','"') # remove double quotes
+    text = re.sub(r"[\s]+",' ',text) # remove duplicate spaces
+    return [party,text]
 
 data = [ list(map(partial(label_files,party), listdir(party))) for party in parties ]
 data = sum(sum(data,[]),[])
 shuffle(data)
 data = map(sanitize_line,data)
 data = list(filter(lambda x: x[1] != ' ', data))
+
 size = len(data)
 training = data[0:int(size*percent)]
 testing = data[int(size*percent):size]
+
+training = training[0:training_size]
+testing = testing[0:testing_size]
 
 system("rm {training.csv,testing.csv} &> /dev/null")
 with open("training.csv", 'w') as csvfile:
